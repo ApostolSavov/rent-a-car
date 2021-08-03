ebb160cf4ff57b75eff00f911d675df44cc36d68
Apostol Savov <apostol.savov.97@gmail.com>
2021-08-03 19:15:52 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiURL: 'https://rent-a-car-server.herokuapp.com',
 };
 
